74b3f3d6f32ffa435a1d705c4f9f1e390e1231a6
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-01-31 12:14:46 -0800

    colors rendering from useParams correctly

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,36 @@
 import {
+  Switch,
   Link,
   Redirect,
   BrowserRouter as Router,
   Route,
+  useParams,
 } from 'react-router-dom';
 
 import styles from './App.css';
 
 function RGB() {
-  // 🚨 Get values for r, g, and b from params
-  // 🚨 Uncomment next lines after importing
-  // return (
-  // <div
-  //   className={styles.fill}
-  //   style={{ background: `rgb(${r}, ${g}, ${b})` }}
-  // >
-  //   <p>{`rgb(${r},${g},${b})`}</p>
-  // </div>
-  // );
+  const { r, g, b } = useParams();
+  return (
+    <div
+      className={styles.fill}
+      style={{ background: `rgb(${r}, ${g}, ${b})` }}
+    >
+      <p>{`rgb(${r},${g},${b})`}</p>
+    </div>
+  );
 }
 
 function ScreenColor() {
-  return <div>{/* Create Route Inside Switch */}</div>;
+  return (
+    <div>
+      <Switch>
+        <Route path="/rgb/:r/:g/:b">
+          <RGB />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default function App() {
